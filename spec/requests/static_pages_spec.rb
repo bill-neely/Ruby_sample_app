f494ef459e8a3beabbb0331dad8a634633093ab1
bill-neely <bill.neely.tx@gmail.com>
2014-05-18 10:25:20 -0500

    make a change to see heroku status updates

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "StaticPages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  describe "Home page" do
+  describe "The Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
