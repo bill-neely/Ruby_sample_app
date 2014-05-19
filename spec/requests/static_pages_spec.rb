7ad2e9c7ce58f60b04873b84985510371e4e7559
bill-neely <bill.neely.tx@gmail.com>
2014-05-19 13:26:21 -0500

    Add Users controller.  Add test helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
     before { visit root_path }
     subject { page }
     it { should have_content("Sample App") }
-    it { should have_title("#{base_title}") }
+    it { should have_title(full_title("")) }
     it { should_not have_title("| Home")}
   end
 
@@ -16,21 +16,21 @@ describe "StaticPages" do
     before { visit help_path }
     subject { page }
     it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title("Help")) }
   end
 
   describe "About page" do
     before { visit about_path }
     subject { page }
     it { should have_content('About') }
-    it { should have_title("#{base_title} | About") }
+    it { should have_title(full_title("About")) }
   end
 
   describe "Contact Page" do
     before { visit contact_path }
     subject { page }
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title("Contact")) }
   end
 
 end
