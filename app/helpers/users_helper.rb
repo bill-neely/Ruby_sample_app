24a72a227bd42b6a42c5a47c7f65eacb58e1ab69
bill-neely <bill.neely.tx@gmail.com>
2014-06-07 18:18:37 -0500

    Follow/Unfollow working

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.name, class: "grvatar")
+		image_tag(gravatar_url, alt: user.name, size: "#{size}", class: "grvatar")
 	end
 
 end
